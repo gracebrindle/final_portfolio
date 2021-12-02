36c1a0d1710c850bf682d5528322001fc3c3e2ef
Grace Brindle <89871638+gracebrindle@users.noreply.github.com>
2021-12-02 10:36:14 -0500

    Add files via upload

---
+++ js/hover_color.js
@@ -0,0 +1,134 @@
+﻿document.querySelector(".witness_link").addEventListener("mouseover", function () {
+    document.querySelector(".witness_image img").style.filter = "none";
+    document.querySelector(".witness_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".witness_image img").addEventListener("mouseover", function () {
+    document.querySelector(".witness_image img").style.filter = "none";
+    document.querySelector(".witness_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".witness_image img").addEventListener("mouseout", function () {
+    document.querySelector(".witness_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".witness_image img").style.border = "none";
+})
+
+document.querySelector(".spenser_link").addEventListener("mouseover", function () {
+    document.querySelector(".spenser_image img").style.filter = "none";
+    document.querySelector(".spenser_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".spenser_image img").addEventListener("mouseover", function () {
+    document.querySelector(".spenser_image img").style.filter = "none";
+    document.querySelector(".spenser_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".spenser_image img").addEventListener("mouseout", function () {
+    document.querySelector(".spenser_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".spenser_image img").style.border = "none";
+})
+
+document.querySelector(".london_link").addEventListener("mouseover", function () {
+    document.querySelector(".london_image img").style.filter = "none";
+    document.querySelector(".london_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".london_image img").addEventListener("mouseover", function () {
+    document.querySelector(".london_image img").style.filter = "none";
+    document.querySelector(".london_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".london_image img").addEventListener("mouseout", function () {
+    document.querySelector(".london_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".london_image img").style.border = "none";
+})
+
+document.querySelector(".carleton_link").addEventListener("mouseover", function () {
+    document.querySelector(".carleton_image img").style.filter = "none";
+    document.querySelector(".carleton_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".carleton_image img").addEventListener("mouseover", function () {
+    document.querySelector(".carleton_image img").style.filter = "none";
+    document.querySelector(".carleton_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".carleton_image img").addEventListener("mouseout", function () {
+    document.querySelector(".carleton_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".carleton_image img").style.border = "none";
+})
+
+document.querySelector(".westfield_link").addEventListener("mouseover", function () {
+    document.querySelector(".westfield_image img").style.filter = "none";
+    document.querySelector(".westfield_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".westfield_image img").addEventListener("mouseover", function () {
+    document.querySelector(".westfield_image img").style.filter = "none";
+    document.querySelector(".westfield_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".westfield_image img").addEventListener("mouseout", function () {
+    document.querySelector(".westfield_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".westfield_image img").style.border = "none";
+})
+
+document.querySelector(".wahpekute_link").addEventListener("mouseover", function () {
+    document.querySelector(".wahpekute_image img").style.filter = "none";
+    document.querySelector(".wahpekute_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".wahpekute_image img").addEventListener("mouseover", function () {
+    document.querySelector(".wahpekute_image img").style.filter = "none";
+    document.querySelector(".wahpekute_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".wahpekute_image img").addEventListener("mouseout", function () {
+    document.querySelector(".wahpekute_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".wahpekute_image img").style.border = "none";
+})
+
+document.querySelector(".cfc_link").addEventListener("mouseover", function () {
+    document.querySelector(".cfc_image img").style.filter = "none";
+    document.querySelector(".cfc_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".cfc_image img").addEventListener("mouseover", function () {
+    document.querySelector(".cfc_image img").style.filter = "none";
+    document.querySelector(".cfc_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".cfc_image img").addEventListener("mouseout", function () {
+    document.querySelector(".cfc_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".cfc_image img").style.border = "none";
+})
+
+document.querySelector(".theaters_link").addEventListener("mouseover", function () {
+    document.querySelector(".theaters_image img").style.filter = "none";
+    document.querySelector(".theaters_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".theaters_image img").addEventListener("mouseover", function () {
+    document.querySelector(".theaters_image img").style.filter = "none";
+    document.querySelector(".theaters_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".theaters_image img").addEventListener("mouseout", function () {
+    document.querySelector(".theaters_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".theaters_image img").style.border = "none";
+})
+
+document.querySelector(".brcl_link").addEventListener("mouseover", function () {
+    document.querySelector(".brcl_image img").style.filter = "none";
+    document.querySelector(".brcl_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".brcl_image img").addEventListener("mouseover", function () {
+    document.querySelector(".brcl_image img").style.filter = "none";
+    document.querySelector(".brcl_image img").style.border = "solid black 3px";
+})
+
+document.querySelector(".brcl_image img").addEventListener("mouseout", function () {
+    document.querySelector(".brcl_image img").style.filter = "grayscale(100%)";
+    document.querySelector(".brcl_image img").style.border = "none";
+})
